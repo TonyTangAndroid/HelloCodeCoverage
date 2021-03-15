b90938c902ac7109ce8c6fd94182d097d144ddc2
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-03-15 00:14:49 -0700

    Ignore PrefUtilTest.java

--- library_android/src/androidTest/java/org/neotech/library/android/PrefUtilTest.java
+++ library_android/src/androidTest/java/org/neotech/library/android/PrefUtilTest.java
@@ -3,9 +3,10 @@ package org.neotech.library.android;
 import static org.junit.Assert.assertNotNull;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+@Ignore("test code coverage")
 @RunWith(AndroidJUnit4.class)
 public class PrefUtilTest {
 
