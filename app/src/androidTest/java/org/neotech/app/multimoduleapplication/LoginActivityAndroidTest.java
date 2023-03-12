4bf2a9e0388d09a7d93fc46583853168cfdfff8d
Zhilu Tang <tonythompsoncmu@gmail.com>
2023-03-11 18:29:35 -0800

    Enable more android test

--- app/src/androidTest/java/org/neotech/app/multimoduleapplication/LoginActivityAndroidTest.java
+++ app/src/androidTest/java/org/neotech/app/multimoduleapplication/LoginActivityAndroidTest.java
@@ -7,12 +7,10 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.google.common.truth.Truth;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.neotech.app.LoginActivity;
 
-@Ignore("test code coverage")
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityAndroidTest {
 
