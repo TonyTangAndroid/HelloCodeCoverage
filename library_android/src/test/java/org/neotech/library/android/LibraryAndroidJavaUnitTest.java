c87710695f375cb2ae73b61731b082298dcfa37b
Zhilu Tang <tonythompsoncmu@gmail.com>
2023-03-11 17:19:54 -0800

    Ignore test to surface the new metrics.

--- library_android/src/test/java/org/neotech/library/android/LibraryAndroidJavaUnitTest.java
+++ library_android/src/test/java/org/neotech/library/android/LibraryAndroidJavaUnitTest.java
@@ -2,10 +2,12 @@ package org.neotech.library.android;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class LibraryAndroidJavaUnitTest {
 
+  @Ignore("ignore")
   @Test
   public void touchJavaCodeInLibrary() {
     LibraryAndroidJava.getInstance().touchedByJavaUnitTestInAndroidLibrary();
