b835f71225396357bfefb8dd3f57f881f4b24686
Zhilu Tang <tonythompsoncmu@gmail.com>
2023-03-11 17:30:12 -0800

    Revert "Ignore more test"
    This reverts commit 8fd71ede826abc3e9d5e9f1f02ab2e6aa160ea56.

--- library_roboletric/src/test/java/com/demo/lib1/StudentProviderTest.java
+++ library_roboletric/src/test/java/com/demo/lib1/StudentProviderTest.java
@@ -1,10 +1,8 @@
 package com.demo.lib1;
 
 import com.google.common.truth.Truth;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore("Ignore")
 public class StudentProviderTest {
 
   @Test
--- library_roboletric/src/test/java/com/demo/lib1/TeachProviderTest.java
+++ library_roboletric/src/test/java/com/demo/lib1/TeachProviderTest.java
@@ -8,6 +8,7 @@ public class TeachProviderTest {
 
   @Test
   public void name() {
+    assertThat(1 + 1).isNotEqualTo(3);
     assertThat(TeachProvider.name()).isEqualTo("Tony Teacher");
   }
 }
