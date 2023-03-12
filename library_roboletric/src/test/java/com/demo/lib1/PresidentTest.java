c4ee4d22a8dd65cca94c849ee4689e744864a1b2
Zhilu Tang <tonythompsoncmu@gmail.com>
2023-03-11 17:24:55 -0800

    Ignore more test

--- library_roboletric/src/test/java/com/demo/lib1/PresidentTest.java
+++ library_roboletric/src/test/java/com/demo/lib1/PresidentTest.java
@@ -3,8 +3,10 @@ package com.demo.lib1;
 import static org.junit.Assert.*;
 
 import com.google.common.truth.Truth;
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore("Ignore")
 public class PresidentTest {
 
   @Test
--- library_roboletric/src/test/java/com/demo/lib1/TeachProviderTest.java
+++ library_roboletric/src/test/java/com/demo/lib1/TeachProviderTest.java
@@ -8,7 +8,6 @@ public class TeachProviderTest {
 
   @Test
   public void name() {
-    assertThat(1 + 1).isNotEqualTo(3);
     assertThat(TeachProvider.name()).isEqualTo("Tony Teacher");
   }
 }
